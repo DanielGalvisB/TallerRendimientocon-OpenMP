2a78e567eac5eeb4db5bf18de285e43ee3ae9022
Daniel Galvis <141146254+DanielGalvisB@users.noreply.github.com>
2025-05-20 11:25:12 -0500

    Create main.c

---
+++ main.c
@@ -0,0 +1,36 @@
+// principal.c
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include "biblioteca.h"
+
+int main(int argc, char *argv[]) {
+    if (argc != 3) {
+        printf("Uso: %s <tamMatriz> <numHilos>\n", argv[0]);
+        return 1;
+    }
+
+    int N = atoi(argv[1]);
+    int numHilos = atoi(argv[2]);
+
+    srand(time(NULL));
+
+    double **A = crearMatriz(N);
+    double **B = crearMatriz(N);
+    double **C = crearMatriz(N);
+
+    llenarMatriz(A, N);
+    llenarMatriz(B, N);
+
+    double start = omp_get_wtime();
+    multiplicarMatriz(A, B, C, N, numHilos);
+    double end = omp_get_wtime();
+
+    printf("Tiempo ejecución con %d hilos y tamaño %d: %.6f segundos\n", numHilos, N, end - start);
+
+    liberarMatriz(A, N);
+    liberarMatriz(B, N);
+    liberarMatriz(C, N);
+
+    return 0;
+}
